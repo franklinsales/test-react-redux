b0fbf277e4bbb8103d277ad4d3950b0e489fc42b
Franklin Sales <franklin.salesx@gmail.com>
2022-07-12 11:50:17 -0300

    improve style list of table

--- src/components/shared/TableList/style.ts
+++ src/components/shared/TableList/style.ts
@@ -1,3 +1,29 @@
 import styled from "styled-components";
 
-styled
+export const TableWrapper = styled.table`
+
+    border: solid 1px #DDEEEE;
+    border-collapse: collapse;
+    border-spacing: 0;
+    font-size: 13px;
+    width: 300px;
+
+    margin-top: 20px;
+
+    thead th {
+        background-color: #DDEFEF;
+        border: solid 1px #DDEEEE;
+        color: #336B6B;
+        padding: 10px;
+        text-align: left;
+        text-shadow: 1px 1px 1px #fff;
+    }
+
+    tbody td {
+        border: solid 1px #DDEEEE;
+        color: #333;
+        padding: 10px;
+        text-shadow: 1px 1px 1px #fff;
+    }
+
+`
